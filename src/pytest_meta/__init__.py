eed1d4bed1af1e0519db374a0fe4f7b6b7fde38c
Guillermo Gil <guillegil@proton.me>
2025-08-29 12:45:02 +0200

    Add first file structure

---
+++ src/pytest_meta/__init__.py
@@ -0,0 +1,4 @@
+from .metainfo.metainfo import meta
+
+__version__ = "0.1.0"
+__all__ = ["meta"]
---
+++ tests/test_properties.py
@@ -0,0 +1,6 @@
+import pytest
+# from pytest_meta import meta
+
+@pytest.mark.properties
+def test_properties():
+    print("🧪 Hello test!")
