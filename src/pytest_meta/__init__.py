ae6f06f74b1e521ae36a027bf5bb2a2713510f80
Guillermo Gil <guillegil@proton.me>
2025-09-02 09:16:18 +0200

    Fixtry: Solve possible issue with NoneType class
    - Meta class is not accessible from other plugins
      hopefuly this is a fix!

--- src/pytest_meta/__init__.py
+++ src/pytest_meta/__init__.py
@@ -1,16 +1,24 @@
+from typing import Optional
 from .metainfo.metainfo import MetaInfo
 
 __version__ = "0.1.0"
-__all__ = ["meta"]
+__all__ = ["meta", "get_meta", "_set_meta"]
 
-# This will be the global instance that gets exposed
-meta = None
+# Will be initialized later by the plugin
+meta: Optional[MetaInfo] = None
 
 def get_meta() -> MetaInfo:
-    """Get the current meta instance."""
+    """Return the global MetaInfo instance.
+    Raises if accessed before pytest has initialized it.
+    """
+    if meta is None:
+        raise RuntimeError(
+            "pytest_meta.meta accessed before pytest has created it. "
+            "Did you import it too early?"
+        )
     return meta
 
-def _set_meta(meta_instance: MetaInfo):
-    """Internal function to set the meta instance."""
+def _set_meta(meta_instance: MetaInfo) -> None:
+    """Internal function used by the pytest plugin to set the global instance."""
     global meta
     meta = meta_instance
